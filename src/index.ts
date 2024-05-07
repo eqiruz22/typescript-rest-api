5582dfaff2398988752944ae9c29174910c5560e
settclow <ahmadrifa11620@gmail.com>
2024-05-07 11:13:31 +0700

    add morgan to access log

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express from "express"
+import express,{Request,Response} from "express"
 import dotnev from "dotenv"
 import RoleRoute from "./routes/RoleRoute"
 import UserRoute from './routes/UserRoute'
+import morgan from "morgan"
+import fs from "fs"
+import path from "path"
 dotnev.config()
 const app = express()
 
 app.use(express.json())
+app.use(morgan('dev',{
+  skip:function(req:Request,res:Response){return res.statusCode < 400}
+}))
+const accessLog = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'})
+app.use(morgan('combined',{stream:accessLog}))
 app.use(UserRoute)
 app.use(RoleRoute)
 app.listen(process.env.APP_PORT, () => {
